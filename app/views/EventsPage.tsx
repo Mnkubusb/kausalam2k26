ebce9507e60c31244f0a12b269df1654edfa80a0
Mnkubusb <manikmnr315@gmail.com>
2026-02-23 00:40:53 +0530

    added some changes

--- app/views/EventsPage.tsx
+++ app/views/EventsPage.tsx
@@ -61,7 +61,7 @@ const EventsPage: React.FC<EventsPageProps> = ({ events, onSelectEvent }) => {
       <div className="flex flex-col md:flex-row gap-6 mb-12 relative top-8 z-40 bg-[#050505]/80 backdrop-blur-xl p-4 rounded-3xl border border-white/5">
         <div className="relative flex-1">
           <Search className="absolute left-4 top-1/4 -translate-y-1/2 text-gray-500" size={20} />
-          <input 
+        <input 
             type="text"
             placeholder="Search events..."
             value={searchQuery}
